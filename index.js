af33ab5cb5f0d9362c079552181e9f4bdeb9f45c
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-13 12:00:11 +0530

    intro

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hi, i'm hima");
+console.log("welcome to js learning");
+
+window.alert("be ready, it's hign time to learn");
+
+// single line comments
+
+/*  
+    multiline 
+    comments
+
+*/
