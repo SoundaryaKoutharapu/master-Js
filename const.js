6063c0ff536f0305490ae532dff550eed83a2e72
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-15 11:30:34 +0530

    const

---
+++ const.js
@@ -0,0 +1,12 @@
+// const =  a variable that can't be changed
+
+const PI = 3.14159;
+
+let radius = window.prompt("enter the radius");
+radius = Number(radius);
+ 
+// PI = 12;
+
+let circumference = 2*PI*radius;
+
+console.log("circumference is", circumference);
