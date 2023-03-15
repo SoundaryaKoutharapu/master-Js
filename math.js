d6eeeecc85a64fb66e80d024873dbaa8b0490076
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-15 11:50:30 +0530

    Math

---
+++ math.js
@@ -0,0 +1,27 @@
+
+// provides basic functionality and constants
+
+let w = 8.956;
+
+// w = Math.round(w);
+// w = Math.floor(w);
+// w = Math.ceil(w);
+// w = Math.sqrt(w);
+// w = Math.abs(w);
+// w = Math.pow(w,2);
+
+console.log(w);
+
+var x = 67;
+var y = 56;
+var z = 23;
+
+let max = Math.max(x,y,z);
+let min = Math.min(x,y,z);
+
+console.log(max);
+console.log(min);
+
+let f = Math.PI;
+console.log(f);
+
