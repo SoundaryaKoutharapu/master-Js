ecc4909b19ca92d45771f4175cbea5e247d97146
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-18 09:27:00 +0530

    if-else

---
+++ if-statement.js
@@ -0,0 +1,31 @@
+// if statements  =  a basic form of decision making
+
+/* 
+
+let age = window.prompt("tell me your age");
+
+if(age<0)
+{
+    console.log("you haven't born yet");
+}
+else if(age<18)
+{
+    console.log("you are child");
+}
+else
+{
+    console.log("you are adult")
+}
+
+*/
+
+let online = true;
+
+if(online)
+{
+    console.log("true")
+}
+else
+{
+    console.log("false");
+}
