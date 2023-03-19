37b933429def0db6fbdddd9cbfb32c3473fb7a03
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-19 07:18:16 +0530

    do-while

---
+++ do-while.js
@@ -0,0 +1,10 @@
+// do while loop = do something, then check the condition, repeat if condition is true
+
+let username;
+
+do
+{
+    //window.alert("it's not safe");
+    console.log("print first, check after");
+}
+while(username=="hima");
