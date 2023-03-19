822d243f6c26640021c7d88e28b8ebc9521ab420
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-19 07:35:52 +0530

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,19 @@
+// break =  breaks out of a loop entirely
+// continue = skip an iteration of loop
+
+/* let count;
+for(count = 34; count<=99; count+=10)
+{
+   if(count==54)
+    break;
+    console.log("break the statement")
+}
+*/
+
+let count;
+for(count = 34; count<=99; count+=10)
+{
+   if(count==54)
+   continue;
+    console.log("break the statement");
+}
