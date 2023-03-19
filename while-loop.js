667d0770b96591ae078b34e4b32b67a862dff68e
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-19 07:11:23 +0530

    while-loop

---
+++ while-loop.js
@@ -0,0 +1,19 @@
+// while loop = repeat some code
+// while some condition is true 
+
+
+let username = " ";
+
+while(username == " " || username == null)
+{
+   username = window.prompt("enter your name");   
+}
+
+console.log(username);
+
+/*
+while(0==0)
+{
+    console.log("repeat");
+}
+*/
