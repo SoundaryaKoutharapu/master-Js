72a22add2fe27bb85c9b2b6c8ac63e87d35a9977
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-19 09:43:57 +0530

    added

--- nested-loops.js
+++ nested-loops.js
@@ -1,5 +1,5 @@
 // nested loops = loop inside a loop
-
+/*
 let i, j;
 
 for(i = 1; i<=4; i+=1)
@@ -8,4 +8,18 @@ for(i = 1; i<=4; i+=1)
    {
     console.log("you can do it");
    }
+}
+*/
+
+let symbol = window.prompt("enter a symbol");
+let rows = window.prompt("enter row value");
+let column = window.prompt("enter col value");
+
+for(let i =1; i<=rows;i++)
+{
+   for(let j = 1; j<=column; j++)
+   {
+     document.getElementById("myRectangle").innerHTML +=symbol;
+   }
+     document.getElementById("myRectangle").innerHTML += "<br>";
 }
