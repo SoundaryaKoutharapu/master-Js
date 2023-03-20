7a75f00a8808b5d2a022b1dc6e9811755d201003
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-20 14:35:46 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+// functions = define code once and use it many times
+
+function startProgram()
+{
+
+let age = 23;
+let username = "hima";
+MyBirthday(age,username);
+
+}
+
+startProgram()
+
+function MyBirthday(age,username)
+{
+    console.log("happy birthday to you");
+    console.log("happy birthday to you");
+    console.log("happy birthday dear ");
+    console.log("happy birthday to you", username);
+    console.log("you are", age ,"years old!");
+};
