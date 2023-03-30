7522e7502c865c8e59fcea303c3585cc88cebeb7
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-03-30 07:48:06 +0530

    sort-array

---
+++ sortanarray.js
@@ -0,0 +1,9 @@
+let vegetables = ["tamoto", "cabbage", "potato", "cucumber", "turmeric"];
+
+// sort in asc
+vegetables = vegetables.sort();
+console.log(vegetables);
+
+//sort in reverse/desc
+vegetables = vegetables.sort().reverse();
+console.log(vegetables);
