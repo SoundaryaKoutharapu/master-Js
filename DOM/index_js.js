989ca34123d203558fec945b4e51843db3fcc58f
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-11 12:39:50 +0530

    Dom Intro

---
+++ DOM/index_js.js
@@ -0,0 +1,16 @@
+/* 
+    DOM = Document object model
+          An Interface for changing the content of a page
+*/
+
+//console.log(document);   display DOM     
+//console.dir(document);   list properties of your DOM
+
+// console.log(document.title);  gives title name
+// console.log(document.URL);
+
+// document.title = "change the title";        changes the title
+// document.location = "http://www.google.com";
+
+document.body.style.background = "green";
+document.getElementById("myDiv").innerHTML = "Hello";
