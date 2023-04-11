5369d3b132f8168eded6338ece46012a01a01d9f
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-11 13:39:58 +0530

    element selectors

---
+++ DOM/elementSelectors.js
@@ -0,0 +1,38 @@
+// Get element by Name
+/* 
+let element =  document.getElementById("myTitle");
+element.style.background =  "Seagreen";
+
+
+let items = document.getElementsByName("items");
+console.log(items);
+console.log(items[1]);
+
+items.forEach( (item) => {
+    if(item.checked)
+    {
+        console.log(item.id);
+    }
+})
+*/
+
+
+
+//  Get element by Tag Name
+/*
+let vegetables  = document.getElementsByTagName("li");
+console.log(vegetables[0]);
+
+vegetables[1].style.color = "Red";
+vegetables[0].style.background = "lightgreen"
+*/
+
+
+//  Get element by Class Name
+/*
+let desserts1 = document.getElementsByClassName("dessert");
+console.log(desserts1[0]);
+desserts1[1].style.background = "Red"
+desserts1[0].style.background = "Green"
+*/
+
