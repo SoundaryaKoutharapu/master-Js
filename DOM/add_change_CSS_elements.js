257afa6c370aa01a57d6a0fe5ed3a4b0fdad39e0
Soundarya Koutharapu <soundaryakoutharapu@gmail.com>
2023-04-12 15:54:00 +0530

    add and change CSS elements

---
+++ DOM/add_change_CSS_elements.js
@@ -0,0 +1,14 @@
+
+const title = document.getElementById("myTitle");
+
+title.style.color = "pink";
+//title.style.color = "rgb(55,678,123)";
+//title.style.color = "#5678";
+
+title.style.fontFamily = "consolas";
+title.style.border = "3px solid";
+title.style.maxWidth = "400px";
+title.style.textAlign = "center";
+title.style.backgroundColor = "gray";
+title.style.display = "block";
+//title.style.display = "none";
